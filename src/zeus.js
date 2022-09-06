2bca4a07ec8eb243dea99066d50904e72f5c1df6
igorskyflyer <igor.dvlpr@gmail.com>
2022-09-06 15:56:45 +0200

    Add JSDocs

--- src/zeus.js
+++ src/zeus.js
@@ -1,10 +1,10 @@
 'use strict'
 
 /**
- *
- * @param {string} htmlElement
- * @param {object} [attributes={}]
- * @returns {HTMLElement|null}
+ * Creates an HTML element with the provided attributes.
+ * @param {string} htmlElement The name of the HTML element to create
+ * @param {object} [attributes={}] The attributes to apply to the newly created HTML element
+ * @returns {HTMLElement|null} Returns the expected HTML element or `null` if the parameter *htmlElement* is not a **string**
  */
 function zeus(htmlElement, attributes = {}) {
   if (typeof htmlElement !== 'string') {
